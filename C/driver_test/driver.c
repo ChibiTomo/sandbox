0eec388ebee674a1a7bc8b9689db6c1d9d3abe47
THOMIAS Yannis <thomias.yannis@gmail.com>
2014-06-18 11:42:55 +0200

    Add writes functions

--- C/driver_test/driver.c
+++ C/driver_test/driver.c
@@ -1,5 +1,64 @@
 #include <ntddk.h>
 
+#include "driver.h"
+
+NTSTATUS STDCALL DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING registryPath) {
+	NTSTATUS status = STATUS_SUCCESS; // default status
+
+	// Debug print
+	DbgPrint("Hello!!\n");
+	DbgPrint("RegistryPath=%wZ\n", registryPath);
+
+	// Creation of the associated device
+	PDEVICE_OBJECT deviceObject = NULL; // represents a logical, virtual, or physical device for which a driver handles I/O requests.
+	UNICODE_STRING deviceName;
+	UNICODE_STRING dosDeviceName;// unicode string struct:	USHORT Length, current length of the string
+								//							USHORT MaximumLength, max length of the string
+								//							PWSTR  Buffer, the string /!\not even NULL terminated!
+
+	RtlInitUnicodeString(&deviceName, L"\\Device\\Example"); // Initialize a unicode string
+	RtlInitUnicodeString(&dosDeviceName, L"\\DosDevices\\Example");
+
+	status = IoCreateDevice(driverObject, 			// driver object
+							0, 						// length of device extention (extra data to pass)
+							&deviceName, 			// device name
+							FILE_DEVICE_UNKNOWN, 	// unknow because it's not a specific device
+							FILE_DEVICE_SECURE_OPEN,// flags:	FILE_DEVICE_SECURE_OPEN,
+													//			FILE_FLOPPY_DISKETTE,
+													//			FILE_READ_ONLY_DEVICE,
+													//			FILE_REMOVABLE_MEDIA,
+													//			FILE_WRITE_ONCE_MEDIA
+							FALSE, 					// is an exclusive device?
+							&deviceObject);			// out
+
+	if (status != STATUS_SUCCESS) {
+		IoDeleteDevice(deviceObject);
+		goto cleanup;
+	}
+
+	for (UINT i = 0; i < IRP_MJ_MAXIMUM_FUNCTION; i++) {
+		driverObject->MajorFunction[i] = unsuported_function;
+	}
+	driverObject->MajorFunction[IRP_MJ_CLOSE] = my_close;
+	driverObject->MajorFunction[IRP_MJ_CREATE] = my_create;
+	driverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = my_io_control;
+	driverObject->MajorFunction[IRP_MJ_READ] = my_read;
+	driverObject->MajorFunction[IRP_MJ_WRITE] = USE_WRITE_FUNCTION;
+
+	// Setting my_unload as unload function
+	driverObject->DriverUnload = my_unload;
+
+//	deviceObject->Flags |= IO_TYPE;
+	deviceObject->Flags &= (~DO_DEVICE_INITIALIZING);	// DO_DEVICE_INITIALIZING: tell to not send I/O request to
+														// the device. It is MANDATORY to clear it to use the device.
+														// (except in DriverEntry because it is automatic)
+
+	IoCreateSymbolicLink(&dosDeviceName, &deviceName);
+
+cleanup:
+	return status;
+}
+
 VOID STDCALL my_unload(PDRIVER_OBJECT DriverObject) {
 	DbgPrint("GoodBye!!\n");
 
@@ -45,59 +104,20 @@ NTSTATUS STDCALL my_read(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
 	return status;
 }
 
-NTSTATUS STDCALL DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING registryPath) {
-	NTSTATUS status = STATUS_SUCCESS; // default status
+NTSTATUS STDCALL my_write_direct(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	NTSTATUS status = STATUS_SUCCESS;
+	DbgPrint("my_write_direct called \n");
+	return status;
+}
 
-	// Debug print
-	DbgPrint("Hello!!\n");
-	DbgPrint("RegistryPath=%wZ\n", registryPath);
+NTSTATUS STDCALL my_write_buffered(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	NTSTATUS status = STATUS_SUCCESS;
+	DbgPrint("my_write_buffered called \n");
+	return status;
+}
 
-	// Creation of the associated device
-	PDEVICE_OBJECT deviceObject = NULL; // represents a logical, virtual, or physical device for which a driver handles I/O requests.
-	UNICODE_STRING deviceName;
-	UNICODE_STRING dosDeviceName;// unicode string struct:	USHORT Length, current length of the string
-								//							USHORT MaximumLength, max length of the string
-								//							PWSTR  Buffer, the string /!\not even NULL terminated!
-
-	RtlInitUnicodeString(&deviceName, L"\\Device\\Example"); // Initialize a unicode string
-	RtlInitUnicodeString(&dosDeviceName, L"\\DosDevices\\Example");
-
-	status = IoCreateDevice(driverObject, 			// driver object
-							0, 						// length of device extention (extra data to pass)
-							&deviceName, 			// device name
-							FILE_DEVICE_UNKNOWN, 	// unknow because it's not a specific device
-							FILE_DEVICE_SECURE_OPEN,// flags:	FILE_DEVICE_SECURE_OPEN,
-													//			FILE_FLOPPY_DISKETTE,
-													//			FILE_READ_ONLY_DEVICE,
-													//			FILE_REMOVABLE_MEDIA,
-													//			FILE_WRITE_ONCE_MEDIA
-							FALSE, 					// is an exclusive device?
-							&deviceObject);			// out
-
-	if (status != STATUS_SUCCESS) {
-		IoDeleteDevice(deviceObject);
-		goto cleanup;
-	}
-
-	for (UINT i = 0; i < IRP_MJ_MAXIMUM_FUNCTION; i++) {
-		driverObject->MajorFunction[i] = unsuported_function;
-	}
-	driverObject->MajorFunction[IRP_MJ_CLOSE] = my_close;
-	driverObject->MajorFunction[IRP_MJ_CREATE] = my_create;
-	driverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = my_io_control;
-	driverObject->MajorFunction[IRP_MJ_READ] = my_read;
-	//driverObject->MajorFunction[IRP_MJ_WRITE] = USE_WRITE_FUNCTION;
-
-	// Setting my_unload as unload function
-	driverObject->DriverUnload = my_unload;
-
-//	deviceObject->Flags |= IO_TYPE;
-	deviceObject->Flags &= (~DO_DEVICE_INITIALIZING);	// DO_DEVICE_INITIALIZING: tell to not send I/O request to
-														// the device. It is MANDATORY to clear it to use the device.
-														// (except in DriverEntry because it is automatic)
-
-	IoCreateSymbolicLink(&dosDeviceName, &deviceName);
-
-cleanup:
+NTSTATUS STDCALL my_write_neither(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	NTSTATUS status = STATUS_SUCCESS;
+	DbgPrint("my_write_neither called \n");
 	return status;
 }
