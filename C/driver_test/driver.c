906f11062f0eed0eb90cd858e49a4c38a6d306cf
THOMIAS Yannis <thomias.yannis@gmail.com>
2014-06-18 11:01:28 +0200

    Add function for I/O handling

--- C/driver_test/driver.c
+++ C/driver_test/driver.c
@@ -9,15 +9,42 @@ VOID STDCALL my_unload(PDRIVER_OBJECT DriverObject) {
 	}
 }
 
+NTSTATUS STDCALL unsuported_function(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	NTSTATUS status = STATUS_NOT_SUPPORTED;
+	DbgPrint("unsuported_function called \n");
+	return status;
+}
+
+NTSTATUS STDCALL my_close(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	NTSTATUS status = STATUS_SUCCESS;
+	DbgPrint("my_close called \n");
+	return status;
+}
+
+NTSTATUS STDCALL my_create(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	NTSTATUS status = STATUS_SUCCESS;
+	DbgPrint("my_create called \n");
+	return status;
+}
+
+NTSTATUS STDCALL my_io_control(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	NTSTATUS status = STATUS_SUCCESS;
+	DbgPrint("my_io_control called \n");
+	return status;
+}
+
+NTSTATUS STDCALL my_read(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	NTSTATUS status = STATUS_SUCCESS;
+	DbgPrint("my_read called \n");
+	return status;
+}
+
 NTSTATUS STDCALL DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING registryPath) {
 	NTSTATUS status = STATUS_SUCCESS; // default status
 
 	// Debug print
 	DbgPrint("Hello!!\n");
 	DbgPrint("RegistryPath=%wZ\n", registryPath);
-
-	// Setting my_unload as unload function
-	driverObject->DriverUnload = my_unload;
 
 	// Creation of the associated device
 	PDEVICE_OBJECT deviceObject = NULL; // represents a logical, virtual, or physical device for which a driver handles I/O requests.
@@ -40,10 +67,23 @@ NTSTATUS STDCALL DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING regist
 													//			FILE_WRITE_ONCE_MEDIA
 							FALSE, 					// is an exclusive device?
 							&deviceObject);			// out
+
 	if (status != STATUS_SUCCESS) {
 		IoDeleteDevice(deviceObject);
 		goto cleanup;
 	}
+
+	for (UINT i = 0; i < IRP_MJ_MAXIMUM_FUNCTION; i++) {
+		driverObject->MajorFunction[i] = unsuported_function;
+	}
+	driverObject->MajorFunction[IRP_MJ_CLOSE] = my_close;
+	driverObject->MajorFunction[IRP_MJ_CREATE] = my_create;
+	driverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = my_io_control;
+	driverObject->MajorFunction[IRP_MJ_READ] = my_read;
+	//driverObject->MajorFunction[IRP_MJ_WRITE] = USE_WRITE_FUNCTION;
+
+	// Setting my_unload as unload function
+	driverObject->DriverUnload = my_unload;
 
 cleanup:
 	return status;
