d48af315955119aa975611e627520b2c3c2cfb72
THOMIAS Yannis <thomias.yannis@gmail.com>
2014-07-09 16:56:56 +0200

    Finished

--- C/driver_simple_filter/filter.c
+++ C/driver_simple_filter/filter.c
@@ -1,7 +1,6 @@
 #include <ntddk.h>
-
-#include "filter.h"
 #include "public.h"
+#include "filter.h"
 
 NTSTATUS STDCALL DriverEntry(PDRIVER_OBJECT  driverObject, PUNICODE_STRING registryPath) {
 	NTSTATUS status = STATUS_SUCCESS;
@@ -13,7 +12,7 @@ NTSTATUS STDCALL DriverEntry(PDRIVER_OBJECT  driverObject, PUNICODE_STRING regis
 
 	status = IoCreateDevice(driverObject,
 							sizeof(device_extension_t),
-							NULL,
+							NULL, // Filter driver: device has no name
 							FILE_DEVICE_UNKNOWN,
 							FILE_DEVICE_SECURE_OPEN,
 							FALSE,
@@ -24,6 +23,7 @@ NTSTATUS STDCALL DriverEntry(PDRIVER_OBJECT  driverObject, PUNICODE_STRING regis
 	}
 
 	for (int i = 0; i < IRP_MJ_MAXIMUM_FUNCTION; i++) {
+		// Filter driver: ignore function instead of unsupported
 		driverObject->MajorFunction[i] = my_ignored_function;
 	}
 	driverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = my_ioctl;
@@ -31,6 +31,7 @@ NTSTATUS STDCALL DriverEntry(PDRIVER_OBJECT  driverObject, PUNICODE_STRING regis
 	device_extension_t* device_extension = (device_extension_t*) deviceObject->DeviceExtension;
 	UNICODE_STRING deviceToFilter;
 	RtlInitUnicodeString(&deviceToFilter, DEVICE_PATH);
+	// Attach the filter device in the device stack
 	status = IoAttachDevice(deviceObject, &deviceToFilter, &device_extension->next);
 	if (status != STATUS_SUCCESS) {
 		DbgPrint("Cannot attach device\n");
@@ -39,6 +40,7 @@ NTSTATUS STDCALL DriverEntry(PDRIVER_OBJECT  driverObject, PUNICODE_STRING regis
 
 	PDEVICE_OBJECT filteredDevice = device_extension->next;
 
+	// These flags are used only for ReadFile and WriteFile.
 	deviceObject->Flags |= filteredDevice->Flags & (DO_BUFFERED_IO | DO_DIRECT_IO);
 	deviceObject->DeviceType = filteredDevice->DeviceType;
 	deviceObject->Characteristics = filteredDevice->Characteristics;
@@ -56,9 +58,12 @@ cleanup:
 VOID STDCALL my_unload(PDRIVER_OBJECT driverObject) {
 	DbgPrint("Unload\n");
 
+	// Filter driver: detach device
 	device_extension_t* device_extension = (device_extension_t*)driverObject->DeviceObject->DeviceExtension;
-
 	IoDetachDevice(device_extension->next);
+
+	// No symbolic link to remove
+
 	IoDeleteDevice(driverObject->DeviceObject);
 }
 
@@ -68,23 +73,22 @@ NTSTATUS STDCALL my_ignored_function(PDEVICE_OBJECT deviceObject, PIRP Irp) {
 	PIO_STACK_LOCATION pIoStackIrp = IoGetCurrentIrpStackLocation(Irp);
 	if(!pIoStackIrp) {
 		DbgPrint("Ignored function called without Irp location\n");
-		status = STATUS_UNSUCCESSFUL;
 		goto cleanup;
 	}
 	DbgPrint("Ignored function: 0x%02X\n", pIoStackIrp->MajorFunction);
 
+cleanup:
 	IoSkipCurrentIrpStackLocation(Irp);
 
 	device_extension_t* device_extension = (device_extension_t*)deviceObject->DeviceExtension;
 	status = IoCallDriver(device_extension->next, Irp);
 
-cleanup:
 	return status;
 }
 
 NTSTATUS STDCALL my_ioctl(PDEVICE_OBJECT deviceObject, PIRP Irp) {
 	NTSTATUS status = STATUS_SUCCESS;
-	BOOLEAN passDownIrp = TRUE;
+	BOOLEAN doCompleteIrp = TRUE;
 
 	DbgPrint("my_ioctl called\n");
 
@@ -99,20 +103,27 @@ NTSTATUS STDCALL my_ioctl(PDEVICE_OBJECT deviceObject, PIRP Irp) {
 	switch(pIoStackIrp->Parameters.DeviceIoControl.IoControlCode) {
 		case MY_IOCTL_PUSH:
 			status = my_ioctl_push(deviceObject, Irp);
-			passDownIrp = FALSE;
+			if (status == STATUS_SUCCESS) {
+				// push is asynchrone so we do not complete the Irp
+				// (The filtered will do it for us)
+				doCompleteIrp = FALSE;
+			}
 			break;
 
 		case MY_IOCTL_POP:
 			status = my_ioctl_pop(deviceObject, Irp);
-			passDownIrp = FALSE;
+			// Pop is synchrone so we complete the Irp
 			break;
+
+		default:
+			my_ignored_function(deviceObject, Irp);
+			doCompleteIrp = FALSE;
 	}
 
 cleanup:
-	if (passDownIrp) {
-		IoSkipCurrentIrpStackLocation(Irp);
-		device_extension_t* device_extension = (device_extension_t*)deviceObject->DeviceExtension;
-		status = IoCallDriver(device_extension->next, Irp);
+	if (doCompleteIrp) {
+		Irp->IoStatus.Status = status;
+		IoCompleteRequest(Irp, IO_NO_INCREMENT);
 	}
 	return status;
 }
@@ -129,19 +140,17 @@ NTSTATUS my_ioctl_push(PDEVICE_OBJECT deviceObject, PIRP Irp) {
 		*c -= 32;
 		DbgPrint("Translate: %d=>%c\n", old, *c);
 	} else {
-		status = STATUS_UNSUCCESSFUL;
 		DbgPrint("Error: input is not lower case: %c\n", *c);
+		status = STATUS_UNSUCCESSFUL;
+		Irp->IoStatus.Information = 0;
+		goto cleanup;
 	}
 
-	if (status == STATUS_SUCCESS) {
-		IoSkipCurrentIrpStackLocation(Irp);
-		device_extension_t* device_extension = (device_extension_t*) deviceObject->DeviceExtension;
-		status = IoCallDriver(device_extension->next, Irp);
-	} else { // In case of error
-		Irp->IoStatus.Status = status;
-		Irp->IoStatus.Information = 0;
-		IoCompleteRequest(Irp, IO_NO_INCREMENT);
-	}
+	IoSkipCurrentIrpStackLocation(Irp);
+	device_extension_t* device_extension = (device_extension_t*) deviceObject->DeviceExtension;
+	status = IoCallDriver(device_extension->next, Irp);
+
+cleanup:
 	return status;
 }
 
@@ -155,25 +164,31 @@ NTSTATUS my_ioctl_pop(PDEVICE_OBJECT deviceObject, PIRP Irp) {
 
 	device_extension_t* device_extension = (device_extension_t*) deviceObject->DeviceExtension;
 	status = IoCallDriver(device_extension->next, Irp);
+	if (status != STATUS_SUCCESS) {
+		DbgPrint("Error: filtered driver send back an error: 0x%08X\n", status);
+		goto cleanup;
+	}
 
+	// Remaining actions after the filtered drivers
 	PCHAR c = (PCHAR) Irp->AssociatedIrp.SystemBuffer;
 	char old = *c;
 	if (*c >= 65 && *c <= 65 + 26) {
 		*c += 32;
 		DbgPrint("Translate: %d=>%c\n", old, *c);
+		Irp->IoStatus.Information = 1;
 	} else {
 		status = STATUS_UNSUCCESSFUL;
 		DbgPrint("Error: input is not upper case: %c\n", *c);
 	}
 
-	Irp->IoStatus.Status = status;
-	Irp->IoStatus.Information = 1;
-	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+cleanup:
 	return status;
 }
 
 NTSTATUS my_completion_routine(PDEVICE_OBJECT deviceObject, PIRP Irp, PVOID extension) {
 	DbgPrint("my_completion_routine called\n");
 
+	// This does not complete the Irp so it is possible to continue to use it
+	// after the filtered drivers.
 	return STATUS_MORE_PROCESSING_REQUIRED;
 }
