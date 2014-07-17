824272a84915063574c26024ac6a70e49b8abb61
THOMIAS Yannis <thomias.yannis@gmail.com>
2014-07-17 17:03:04 +0200

    ok

--- C/driver_simple_tdi/driver.c
+++ C/driver_simple_tdi/driver.c
@@ -78,7 +78,7 @@ NTSTATUS STDCALL my_ignored_function(PDEVICE_OBJECT deviceObject, PIRP Irp) {
 		status = STATUS_UNSUCCESSFUL;
 		goto cleanup;
 	}
-	DEBUG("Ignored function: 0x%02X\n", pIoStackIrp->MajorFunction);
+	//DEBUG("Ignored function: 0x%02X\n", pIoStackIrp->MajorFunction);
 
 cleanup:
 	IoSkipCurrentIrpStackLocation(Irp);
@@ -91,6 +91,7 @@ cleanup:
 
 NTSTATUS STDCALL my_internal_ioctl(PDEVICE_OBJECT deviceObject, PIRP Irp) {
 	NTSTATUS status = STATUS_SUCCESS;
+	BOOLEAN passDownIrp = TRUE;
 
 	DEBUG("my_internal_ioctl\n");
 
@@ -103,7 +104,8 @@ NTSTATUS STDCALL my_internal_ioctl(PDEVICE_OBJECT deviceObject, PIRP Irp) {
 
 	switch (pIoStackIrp->MinorFunction) {
 		case TDI_CONNECT:
-			DEBUG("TDI_CONNECT via TCP\n");
+			status = process_tdi_connect(deviceObject, Irp);
+			passDownIrp = FALSE;
 			break;
 
 		case TDI_SEND:
@@ -115,9 +117,57 @@ NTSTATUS STDCALL my_internal_ioctl(PDEVICE_OBJECT deviceObject, PIRP Irp) {
 	}
 
 cleanup:
-	IoSkipCurrentIrpStackLocation(Irp);
-
-	extension_t* extension = (extension_t*) deviceObject->DeviceExtension;
-	status = IoCallDriver(extension->topOfDeviceStack, Irp );
+	if (passDownIrp) {
+		IoSkipCurrentIrpStackLocation(Irp);
+		extension_t* extension = (extension_t*) deviceObject->DeviceExtension;
+		status = IoCallDriver(extension->topOfDeviceStack, Irp );
+	}
 	return status;
+}
+
+NTSTATUS process_tdi_connect(PDEVICE_OBJECT deviceObject, PIRP Irp) {
+	NTSTATUS status = STATUS_SUCCESS;
+
+	DEBUG("TDI_CONNECT via TCP\n");
+
+	// If we are here, there is a stack location
+	PIO_STACK_LOCATION pIoStackIrp = IoGetCurrentIrpStackLocation(Irp);
+
+	PTDI_REQUEST_KERNEL_CONNECT TDI_connectRequest =
+			(PTDI_REQUEST_KERNEL_CONNECT) &(pIoStackIrp->Parameters);
+	PTA_ADDRESS TA_Address_data =
+			((PTRANSPORT_ADDRESS) TDI_connectRequest->RequestConnectionInformation->RemoteAddress)->Address;
+	PTDI_ADDRESS_IP TDI_data = (PTDI_ADDRESS_IP) TA_Address_data->Address;
+
+	// Better to do it now: we must process the Irp as quick as possible
+	IoSkipCurrentIrpStackLocation(Irp);
+	extension_t* extension = (extension_t*) deviceObject->DeviceExtension;
+	DEBUG("TDI_CONNECT: Pass down Irp\n");
+	status = IoCallDriver(extension->topOfDeviceStack, Irp );
+	DEBUG("TDI_CONNECT: Back to processing\n");
+
+	DEBUG("Address type: %d\n", TA_Address_data->AddressType);
+	DEBUG("Address length: %d\n", TA_Address_data->AddressLength);
+
+	network_address_t network_address;
+	convert_to_network_address(&network_address, TDI_data);
+
+	DEBUG("TCP address: %i.%i.%i.%i:%i\n", 	network_address.address[0],
+											network_address.address[0],
+											network_address.address[0],
+											network_address.address[0],
+											network_address.port);
+	return status;
+}
+
+void convert_to_network_address(network_address_t* network_address, PTDI_ADDRESS_IP address_ip) {
+	network_address->address[0] = address_ip->in_addr & 0x000000FF;
+	network_address->address[1] = (address_ip->in_addr>>8) & 0x000000FF,
+	network_address->address[2] = (address_ip->in_addr>>16) & 0x000000FF,
+	network_address->address[3] = (address_ip->in_addr>>24) & 0x000000FF,
+
+	// Translate port from network order to host order (little endian)
+	network_address->port = address_ip->sin_port & 0x00FF;
+	network_address->port = network_address->port<<8;
+	network_address->port += address_ip->sin_port>>8 & 0x00FF;
 }
--- C/driver_simple_tdi/driver.h
+++ C/driver_simple_tdi/driver.h
@@ -9,8 +9,16 @@ typedef struct {
 	PDEVICE_OBJECT topOfDeviceStack ;
 } extension_t;
 
+typedef struct {
+	unsigned char address[4];
+	unsigned short port;
+} network_address_t;
+
 VOID STDCALL my_unload(PDRIVER_OBJECT driverObject);
 NTSTATUS STDCALL my_ignored_function(PDEVICE_OBJECT deviceObject, PIRP Irp);
 NTSTATUS STDCALL my_internal_ioctl(PDEVICE_OBJECT deviceObject, PIRP Irp);
+
+NTSTATUS process_tdi_connect(PDEVICE_OBJECT deviceObject, PIRP Irp);
+void convert_to_network_address(network_address_t* network_address, PTDI_ADDRESS_IP address_ip);
 
 #endif // DRIVER_H
